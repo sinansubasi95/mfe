6049a51259159de73c0bb1350420e91c8e52399a
sinansubasi <sinan95@gmail.com>
2021-02-14 19:50:20 +0300

    container to child communication

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -7,7 +7,7 @@ export default () => {
     const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current, {
+        const { onParentNavigate } = mount(ref.current, {
             onNavigate: ({ pathname: nextPathname }) => {
                 const { pathname } = history.location;
 
@@ -17,7 +17,9 @@ export default () => {
 
             }
         });
-    });
+
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref} />
 }
